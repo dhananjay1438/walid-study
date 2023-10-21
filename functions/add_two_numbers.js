8f6ff8b3c732933fc9083939952b13d4a1a5fa06
Dhananjay Panage <dhananjay@Dhananjays-MacBook-Air.local>
2023-10-22 01:02:55 +0530

    some more code

---
+++ functions/add_two_numbers.js
@@ -0,0 +1,11 @@
+
+function sum(a, b) {
+    console.log(a+b);
+}
+
+var a = 10;
+var b = 20;
+
+// Calling the function
+
+sum(a, b)
---
+++ functions/return_from_function.js
@@ -0,0 +1,15 @@
+function sum(a, b) {
+    var c = a + b;
+
+    return c;
+    // Anything written below return statement will not be
+    // executed
+    console.log("helo world")
+}
+
+var a = 10;
+var b = 20;
+
+var c = sum(a, b)
+console.log(c)
+
